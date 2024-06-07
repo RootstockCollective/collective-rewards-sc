769cf559d7c82ef6fbb0321c8efd792641ea5246
Antonio <antonio@iovlabs.org>
2024-06-07 17:04:38 +0200

    chore: set fixed solidity version

--- test/Foo.t.sol
+++ test/Foo.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.20;
+pragma solidity 0.8.20;
 
 import { Test } from "forge-std/src/Test.sol";
 import { console2 } from "forge-std/src/console2.sol";
