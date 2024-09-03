4f9bd8000995563649012254348f7a0113bbf5d1
fruiz08 <89082649+fruiz08@users.noreply.github.com>
2024-09-03 12:22:03 -0300

    fix(deploy): create env private file (#49)

--- test/mock/UpgradesMocks.sol
+++ test/mock/UpgradesMocks.sol
@@ -67,6 +67,6 @@ contract ChangeExecutorUpgradeMock is ChangeExecutor, UpgradeableMock {
  */
 contract SimplifiedRewardDistributorUpgradeMock is SimplifiedRewardDistributor, UpgradeableMock {
     function getCustomMockValue() external view override returns (uint256) {
-        return newVariable + uint256(uint160(governor));
+        return newVariable + uint256(uint160(governor()));
     }
 }
