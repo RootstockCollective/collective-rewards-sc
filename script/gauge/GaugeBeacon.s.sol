bf7982e6139af112af5c6a4a1c10ae4810b08423
DinizSa <diniz.araujosa@rootstocklabs.com>
2024-10-25 00:10:11 +0100

    feat: deploy with new governance approach

--- script/gauge/GaugeBeacon.s.sol
+++ script/gauge/GaugeBeacon.s.sol
@@ -4,22 +4,20 @@ pragma solidity 0.8.20;
 import { Broadcaster } from "script/script_utils/Broadcaster.s.sol";
 import { Gauge } from "src/gauge/Gauge.sol";
 import { GaugeBeacon } from "src/gauge/GaugeBeacon.sol";
+import { IGoverned } from "../../src/interfaces/IGoverned.sol";
 
 contract Deploy is Broadcaster {
     function run() public returns (GaugeBeacon) {
-        address _changeExecutorAddress = vm.envOr("ChangeExecutorRootstockCollective", address(0));
-        if (_changeExecutorAddress == address(0)) {
-            _changeExecutorAddress = vm.envAddress("CHANGE_EXECUTOR_ADDRESS");
-        }
-        return run(_changeExecutorAddress);
+        address _governed = vm.envAddress("ACCESS_CONTROL_ADDRESS");
+        return run(_governed);
     }
 
-    function run(address changeExecutor_) public broadcast returns (GaugeBeacon) {
-        require(changeExecutor_ != address(0), "Change executor address cannot be empty");
+    function run(address governed_) public broadcast returns (GaugeBeacon) {
+        require(governed_ != address(0), "Change executor address cannot be empty");
         address _gaugeImplementation = address(new Gauge());
         if (vm.envOr("NO_DD", false)) {
-            return new GaugeBeacon(changeExecutor_, _gaugeImplementation);
+            return new GaugeBeacon(IGoverned(governed_), _gaugeImplementation);
         }
-        return new GaugeBeacon{ salt: _salt }(changeExecutor_, _gaugeImplementation);
+        return new GaugeBeacon{ salt: _salt }(IGoverned(governed_), _gaugeImplementation);
     }
 }
