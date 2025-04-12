375af557106d5ab61d71ebed90c73c8ac57d811e
Hakob23 <36648940+Hakob23@users.noreply.github.com>
2025-04-12 14:32:58 +0300

    Tok 679/erc20 approve not validated (#190)
    * refactor: function reverts if, for some reason, the erc20 token does not approve the amount
    
    * fix: adding exclamation mark to the condition
    
    * refactor: overrode MockERC20:approve function and added a test for BackersManagerRootstockCollective:rewardTokenApprove
    
    * Update test/BackersManagerRootstockCollective.t.sol
    
    Co-authored-by: Antonio Morrone <antonio@iovlabs.org>
    
    ---------
    
    Co-authored-by: Antonio Morrone <antonio@iovlabs.org>

--- test/mock/ERC20Mock.sol
+++ test/mock/ERC20Mock.sol
@@ -7,7 +7,7 @@ string constant DEFAULT_NAME = "ERC20Mock";
 string constant DEFAULT_SYMBOL = "E20M";
 
 contract ERC20Mock is ERC20 {
-    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) { }
+    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
 
     function mint(address account_, uint256 amount_) external {
         _mint(account_, amount_);
@@ -15,5 +15,15 @@ contract ERC20Mock is ERC20 {
 
     function burn(address account_, uint256 amount_) external {
         _burn(account_, amount_);
+    }
+
+    function approve(address spender, uint256 value) public override returns (bool) {
+        // Prevent potential race condition (optional but safer. Used in legacy erc20 tokens)
+        if (value != 0 && allowance(msg.sender, spender) != 0) {
+            return false;
+        }
+        _approve(msg.sender, spender, value);
+        emit Approval(msg.sender, spender, value);
+        return true;
     }
 }
