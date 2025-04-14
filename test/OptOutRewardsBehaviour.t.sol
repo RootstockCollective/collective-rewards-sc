fe1e231a217072ed7125f9b9ea9592e601aeb368
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-04-14 11:15:54 +0100

    feat: optOut backers allcoation impact only builders

--- test/OptOutRewardsBehaviour.t.sol
+++ test/OptOutRewardsBehaviour.t.sol
@@ -62,19 +62,6 @@ contract OptOutRewardsBehaviour is BaseTest {
     }
 
     /**
-     * SCENARIO: OptOut Backer cannot allocate
-     */
-    function test_OptOutAllocateRevert() public {
-        // GIVEN alice OptOut
-        vm.startPrank(alice);
-        backersManager.optOutRewards(alice);
-        //  WHEN alice tries to allocate
-        //   THEN tx reverts because OptedOut
-        vm.expectRevert(BackersManagerRootstockCollective.BackerOptedOutRewards.selector);
-        backersManager.allocate(gauge, 0.1 ether);
-    }
-
-    /**
      * SCENARIO: Backer can only OptOut if has no allocations
      */
     function test_OptOutWithAllocation() public {
@@ -94,21 +81,80 @@ contract OptOutRewardsBehaviour is BaseTest {
     }
 
     /**
-     * SCENARIO: OptIn Backer can allocate
+     * SCENARIO: OptOut Backer cannot claim
      */
-    function test_OptInAndAllocate() public {
-        // GIVEN alice OptOut
+    function test_OptOutClaimRevert() public {
+        // GIVEN alice OptOut of rewards
         vm.startPrank(alice);
         backersManager.optOutRewards(alice);
-        //  WHEN alice tries to allocate
+        // AND it allocates
+        backersManager.allocate(gauge, 0.1 ether);
+
+        //  WHEN alice tries to claim
         //   THEN tx reverts because OptedOut
         vm.expectRevert(BackersManagerRootstockCollective.BackerOptedOutRewards.selector);
+        gauge.claimBackerReward(alice);
+    }
+
+    /**
+     * SCENARIO: OptedOut backer can allocate but does not receive rewards and does not affect other backers rewards
+     */
+    function test_OptOutAllocationsNotImpactBackersRewards() public {
+        // GIVEN alice is OptOut
+        vm.prank(alice);
+        backersManager.optOutRewards(alice);
+        //  AND alice allocates
+        vm.prank(alice);
         backersManager.allocate(gauge, 0.1 ether);
-        //  WHEN alice OptIn
-        backersManager.optInRewards(alice);
-        //  THEN alice can allocate
-        vm.expectEmit();
-        emit NewAllocation(alice, address(gauge), 0.1 ether);
+        //  AND bob allocates
+        vm.prank(bob);
         backersManager.allocate(gauge, 0.1 ether);
+
+        // WHEN the cycle ends and the rewards are distributed
+        uint256 _distributedAmountRif = 10 ether;
+        uint256 _distributedAmountCoinbase = 10 ether;
+        _distribute(_distributedAmountRif, _distributedAmountCoinbase);
+        _skipAndStartNewCycle();
+
+        // THEN bob receives half the rewards (gauge backers percentage is 50%)
+        vm.prank(bob);
+        backersManager.claimBackerRewards(gaugesArray);
+        assertApproxEqAbs(rewardToken.balanceOf(bob), _distributedAmountRif / 2, 1);
+        assertApproxEqAbs(bob.balance, _distributedAmountCoinbase / 2, 1);
+    }
+
+    /**
+     * SCENARIO: OptedOut backer allocattions affects builders rewards
+     */
+    function test_OptOutAllocationsImpactBuilderRewards() public {
+        // GIVEN alice is OptOut
+        vm.prank(alice);
+        backersManager.optOutRewards(alice);
+        //  AND alice allocates to gauge
+        vm.prank(alice);
+        backersManager.allocate(gauge, 0.1 ether);
+        //  AND bob allocates to 2 gauges
+        vm.prank(bob);
+        backersManager.allocate(gauge, 0.1 ether);
+        vm.prank(bob);
+        backersManager.allocate(gauge2, 0.1 ether);
+
+        // WHEN the cycle ends and the rewards are distributed
+        uint256 _distributedAmountRif = 10 ether;
+        uint256 _distributedAmountCoinbase = 10 ether;
+        _distribute(_distributedAmountRif, _distributedAmountCoinbase);
+        _skipAndStartNewCycle();
+
+        // AND the builders claims the rewards
+        uint256 _buildersRifReward = _distributedAmountRif / 2; // 50% of the rewards
+        uint256 _buildersCoinbaseReward = _distributedAmountCoinbase / 2;
+        vm.prank(builder);
+        gauge.claimBuilderReward();
+        vm.prank(builder2);
+        gauge2.claimBuilderReward();
+
+        // THEN builder should receive 2/3 of all builder rewards, proportional to the total allocations
+        assertApproxEqAbs(rewardToken.balanceOf(builder), (_buildersRifReward * 2) / 3, 1);
+        assertApproxEqAbs(rewardToken.balanceOf(builder2Receiver), (_buildersCoinbaseReward * 1) / 3, 1);
     }
 }
