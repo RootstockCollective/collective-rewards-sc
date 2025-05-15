2cf96779507e65ddac05a566e7f5c3de45580466
Hakob23 <hakob.arshakyan23@gmail.com>
2025-05-15 19:58:09 +0300

    refactor: gauge introducing new versions for sendRewards functions

--- src/RewardDistributorRootstockCollective.sol
+++ src/RewardDistributorRootstockCollective.sol
@@ -113,29 +113,36 @@ contract RewardDistributorRootstockCollective is UpgradeableRootstockCollective 
     /**
      * @notice sends rewards to backersManager contract to be distributed to the gauges
      * @dev reverts if is not called by foundation treasury address
-     * @param amountERC20_ amount of ERC20 reward token to send
+     * @param amountsERC20_ amount of ERC20 reward token to send
      * @param amountCoinbase_ amount of Coinbase reward token to send
      */
-    function sendRewards(uint256 amountERC20_, uint256 amountCoinbase_) external payable onlyFoundationTreasury {
-        _sendRewards(amountERC20_, amountCoinbase_);
-    }
-
-    /**
-     * @notice sends rewards to backersManager contract and starts the distribution to the gauges
-     * @dev reverts if is not called by foundation treasury address
-     *  reverts if is not in the distribution window
-     * @param amountERC20_ amount of ERC20 reward token to send
-     * @param amountCoinbase_ amount of Coinbase reward token to send
-     */
-    function sendRewardsAndStartDistribution(
-        uint256 amountERC20_,
+    function sendRewards(
+        uint256[] memory amountsERC20_,
         uint256 amountCoinbase_
     )
         external
         payable
         onlyFoundationTreasury
     {
-        _sendRewards(amountERC20_, amountCoinbase_);
+        _sendRewards(amountsERC20_, amountCoinbase_);
+    }
+
+    /**
+     * @notice sends rewards to backersManager contract and starts the distribution to the gauges
+     * @dev reverts if is not called by foundation treasury address
+     *  reverts if is not in the distribution window
+     * @param amountsERC20_ amount of ERC20 reward token to send
+     * @param amountCoinbase_ amount of Coinbase reward token to send
+     */
+    function sendRewardsAndStartDistribution(
+        uint256[] memory amountsERC20_,
+        uint256 amountCoinbase_
+    )
+        external
+        payable
+        onlyFoundationTreasury
+    {
+        _sendRewards(amountsERC20_, amountCoinbase_);
         backersManager.startDistribution();
     }
 
@@ -180,12 +187,15 @@ contract RewardDistributorRootstockCollective is UpgradeableRootstockCollective 
 
     /**
      * @notice internal function to send rewards to backersManager contract
-     * @param amountERC20_ amount of ERC20 reward token to send
+     * @param amountsERC20_ amount of ERC20 reward token to send
      * @param amountCoinbase_ amount of Coinbase reward token to send
      */
-    function _sendRewards(uint256 amountERC20_, uint256 amountCoinbase_) internal {
-        rewardToken.approve(address(backersManager), amountERC20_);
-        backersManager.notifyRewardAmount{ value: amountCoinbase_ }(amountERC20_);
+    function _sendRewards(uint256[] memory amountsERC20_, uint256 amountCoinbase_) internal {
+        for (uint256 i = 0; i < rewardsTokens.length; i++) {
+            IERC20(rewardsTokens[i]).approve(address(backersManager), amountsERC20_[i]);
+        }
+        backersManager.notifyRewardAmountERC20(rewardsTokens, amountsERC20_);
+        backersManager.notifyRewardAmountCoinBase{ value: amountCoinbase_ }();
     }
 
     /**
