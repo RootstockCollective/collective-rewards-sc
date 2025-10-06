f42a3c7ac7e1132d04fcb3bf60b5cd7e2659ca10
DinizSa <diniz.araujosa@rootstocklabs.com>
2025-10-06 18:14:49 +0100

    chore: update evm version to cancun to enable transient storage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,7 +9,7 @@ const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.24",
     settings: {
-      evmVersion: "london",
+      evmVersion: "cancun",
       optimizer: {
         enabled: true,
         runs: 10000,
